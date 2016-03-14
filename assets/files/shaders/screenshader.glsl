6a8b5cfa17373ae86849aff54c53b4eabad859e3
Nico May <nico.may99@gmail.com>
2016-03-13 21:35:22 -0700

    finally a lowres base to start the game on jesus christ

--- assets/files/shaders/screenshader.glsl
+++ assets/files/shaders/screenshader.glsl
@@ -8,7 +8,7 @@ uniform sampler2D tex0;
 void main() {
 	vec2 uv = tcoord;
 
-	vec4 final = texture2D(tex0, vec2(uv.x, uv.y+sin(uv.x)));
+	vec4 final = texture2D(tex0, uv);
 
-    gl_FragColor = final;
+    gl_FragColor = vec4(uv.x,final.g,0,1);//+ uv.xyxy;
 }
--- src/Main.hx
+++ src/Main.hx
@@ -4,65 +4,49 @@ import luxe.Input;
 import luxe.Sprite;
 import luxe.Vector;
 import luxe.Color;
+import luxe.Camera;
+import luxe.Screen;
 
 import phoenix.RenderTexture;
 import phoenix.Batcher;
-import phoenix.Camera;
 import phoenix.Shader;
+import phoenix.Texture;
 
 class Main extends luxe.Game {
 
-	var _canvas: RenderTexture;
-	var _canvasSprite: Sprite;
-	var _canvasShader: Shader;
-	var _sceneCamera: Camera;
-	public var shaderBatch: Batcher;
-
-	var test: Sprite;
+	var player: Sprite;
+	var gameResolution: Vector = new Vector(160,90);
+	var zoom: Int = 4;
 
     override function config(config:luxe.AppConfig) {
 
+		config.window.width = Math.floor(gameResolution.x * zoom);
+		config.window.height = Math.floor(gameResolution.y * zoom);
         return config;
 
     }
 
     override function ready() {
+		Texture.default_filter = FilterType.nearest;
 		Luxe.renderer.clear_color = new Color(0,0,0);
 
-		_canvas = new RenderTexture({id: 'canvas'});
-		_sceneCamera = new Camera();
+		setupScreen();
+		setupBorders();
 
-		shaderBatch = Luxe.renderer.create_batcher({
-			name: 'shaderbatcher',
-			no_add: true,
-			camera: _sceneCamera
-		});
-
-		Luxe.camera.size = new Vector(160,90);
-
-		Luxe.resources.load_shader('postprocess', {frag_id: 'assets/files/shaders/screenshader.glsl', vert_id: 'default'}).then(function(shader) {
-			_canvasShader = shader;
-			_canvasSprite = new Sprite({
-				centered: false,
-				pos: new Vector(0, 0),
-				size: new Vector(Luxe.camera.size.x, Luxe.camera.size.y),
-				texture: _canvas,
-				shader: _canvasShader
-			});
-		});
-
-		test = new Sprite({
-			pos: Luxe.camera.center.clone(),
-			size: new Vector(160,90),
-			batcher: shaderBatch
-		});
-		var test2 = new Sprite({
-			pos: Luxe.camera.center.clone(),
-			size: new Vector(160,90),
-			batcher: shaderBatch
-		});
-		test2.pos.x += 40;
-		test2.pos.y += 40;
+		player = new Sprite({
+		   pos: Luxe.camera.center.clone(),
+		   size: new Vector(11,11),
+		   centered: false
+	   	});
+	   	new Sprite({
+		  	pos: new Vector(),
+			centered: false,
+		  	size: new Vector(160,90),
+			depth: -100,
+			color: new Color(0.2,0.2,0.2)
+	  	});
+	   	x = player.pos.x;
+	   	y = player.pos.y;
     }
 
     override function onkeyup( e:KeyEvent ) {
@@ -73,15 +57,76 @@ class Main extends luxe.Game {
 
     }
 
-    override function update(dt:Float) {
+	override function onwindowsized(e: WindowEvent) {
+		setupScreen();
+	}
 
+	function setupScreen() {
+		Luxe.camera.size = new Vector(gameResolution.x, gameResolution.y);
+		Luxe.camera.viewport.set(0, 0, Luxe.screen.w, Luxe.screen.h);
+	}
+
+	function setupBorders() {
+		new Sprite({
+			centered: false,
+			pos: new Vector(gameResolution.x, -500),
+			size: new Vector(1000, gameResolution.y+1000),
+			color: new Color(0,0,0),
+			depth: 1000
+		});
+
+		new Sprite({
+			centered: false,
+			pos: new Vector(-1000, -500),
+			size: new Vector(1000, gameResolution.y+1000),
+			color: new Color(0,0,0),
+			depth: 1000
+		});
+
+		new Sprite({
+			centered: false,
+			pos: new Vector(0, -1000),
+			size: new Vector(gameResolution.x, 1000),
+			color: new Color(0,0,0),
+			depth: 1000
+		});
+
+		new Sprite({
+			centered: false,
+			pos: new Vector(0, gameResolution.y),
+			size: new Vector(gameResolution.x, 1000),
+			color: new Color(0,0,0),
+			depth: 1000
+		});
+	}
+
+	var x: Float = 0;
+	var y: Float = 0;
+	var g = 600;
+	var s = 40;
+	var velocity: Vector = new Vector();
+    override function update(dt:Float) {
+		velocity.y += dt * g;
+		if(Luxe.input.keydown(Key.left)) {
+			x -= s*dt;
+		}
+		if(Luxe.input.keydown(Key.right)) {
+			x += s*dt;
+		}
+		if(y + player.size.y >= gameResolution.y && Luxe.input.keypressed(Key.up)) {
+			velocity.y = -200;
+		}
+		y += velocity.y * dt;
+		if(y + player.size.y > gameResolution.y) {
+			y = gameResolution.y - player.size.y;
+			velocity.y = 0;
+		}
+
+		player.pos.x = Math.floor(x);
+		player.pos.y = Math.floor(y);
     }
 
 	var _transparent: Color = new Color(0,0,0,0);
 	override function onrender() {
-		Luxe.renderer.target = _canvas;
-		Luxe.renderer.clear(_transparent);
-		shaderBatch.draw();
-		Luxe.renderer.target = null;
 	}
 }
